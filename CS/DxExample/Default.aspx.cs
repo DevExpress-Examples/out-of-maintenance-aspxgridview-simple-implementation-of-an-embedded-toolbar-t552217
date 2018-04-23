a68c403dbbe77463edc3bf5c40b98540eade1d78
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/DxExample/Default.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DxExample {
+    public partial class Default : System.Web.UI.Page {
+        protected void Page_Load(object sender, EventArgs e) {
+
+        }
+
+        protected void Grid_ToolbarItemClick(object source, DevExpress.Web.Data.ASPxGridViewToolbarItemClickEventArgs e) {
+            switch (e.Item.Name) {
+                case "ExportToPDF":
+                    Exporter.WritePdfToResponse();
+                    break;
+                case "ExportToXLS":
+                    Exporter.WriteXlsToResponse();
+                    break;
+                case "ExportToXLSX":
+                    Exporter.WriteXlsxToResponse();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        protected void Grid_RowDeleting(object sender, DevExpress.Web.Data.ASPxDataDeletingEventArgs e) {
+            throw new Exception("CRUD opeartions are not supported");
+        }
+
+        protected void Grid_RowUpdating(object sender, DevExpress.Web.Data.ASPxDataUpdatingEventArgs e) {
+            throw new Exception("CRUD opeartions are not supported");
+        }
+
+        protected void Grid_RowInserting(object sender, DevExpress.Web.Data.ASPxDataInsertingEventArgs e) {
+            throw new Exception("CRUD opeartions are not supported");
+        }
+    }
+}
---
+++ VB/DxExample/Default.aspx.vb
@@ -0,0 +1,28 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Web
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+
+Namespace DxExample
+    Partial Public Class [Default]
+        Inherits System.Web.UI.Page
+
+        Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+
+        End Sub
+
+        Protected Sub Grid_ToolbarItemClick(ByVal source As Object, ByVal e As DevExpress.Web.Data.ASPxGridViewToolbarItemClickEventArgs)
+            Select Case e.Item.Name
+                Case "ExportToPDF"
+                    Exporter.WritePdfToResponse()
+                Case "ExportToXLS"
+                    Exporter.WriteXlsToResponse()
+                Case "ExportToXLSX"
+                    Exporter.WriteXlsxToResponse()
+                Case Else
+            End Select
+        End Sub
+    End Class
+End Namespace
